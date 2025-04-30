5bad899823446688112fc0e0eb9e6689a2649051
nabilmudz <nabilmudzaki306@gmail.com>
2025-04-30 13:59:34 +0700

    Stop tracking prisma migrations

--- prisma/migrations/20250428133736_update_transaction/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE `transaction` ADD CONSTRAINT `transaction_memberId_fkey` FOREIGN KEY (`memberId`) REFERENCES `member`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/migrations/20250430020621_role_add_identifier/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `identifier` to the `role` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `role` ADD COLUMN `identifier` VARCHAR(191) NOT NULL;
