9be5a5d49189fdde6c395fb01e2d2eee1a41409f
Razdan <razdan@curaweda.id>
2025-07-10 10:25:35 +0700

    update

--- src/core/member/member.service.js
+++ src/core/member/member.service.js
@@ -1,186 +1,113 @@
-import BaseService from '../../base/service.base.js';
-import memberConstant from '../../config/member.js';
+import BaseService from "../../base/service.base.js";
+import memberConstant from "../../config/member.js";
 import prisma from '../../config/prisma.db.js';
-import { BadRequest } from '../../exceptions/catch.execption.js';
+import { BadRequest } from "../../exceptions/catch.execption.js";
+import paymentService from "../payment/payment.service.js";
 
 class memberService extends BaseService {
-	constructor() {
-		super(prisma);
-	}
+  constructor() {
+    super(prisma);
+    this.paymentService = new paymentService()
+  }
 
-	findAll = async (query) => {
-		const q = this.transformBrowseQuery(query);
-		const data = await this.db.member.findMany({ ...q });
+  findAll = async (query) => {
+    const q = this.transformBrowseQuery(query);
+    const data = await this.db.member.findMany({ ...q });
 
-		if (query.paginate) {
-			const countData = await this.db.member.count({ where: q.where });
-			return this.paginate(data, countData, q);
-		}
-		return data;
-	};
+    if (query.paginate) {
+      const countData = await this.db.member.count({ where: q.where });
+      return this.paginate(data, countData, q);
+    }
+    return data;
+  };
 
-	findById = async (id) => {
-		const data = await this.db.member.findUnique({ where: { id } });
-		return data;
-	};
+  findById = async (id) => {
+    const data = await this.db.member.findUnique({ where: { id } });
+    return data;
+  };
 
-	create = async (payload) => {
-		const data = await this.db.member.create({ data: payload });
-		return data;
-	};
+  create = async (payload) => {
+    const data = await this.db.member.create({ data: payload });
+    return data;
+  };
 
-	patchVerified = async (id, payload) => {
-		const data = await this.db.member.update({
-			where: { id },
-			data: { dataVerified: payload.verified },
-		});
-		return data;
-	};
+  patchVerified = async (id, payload) => {
+    const data = await this.db.member.update({ where: { id }, data: { dataVerified: payload.verified } });
+    return data;
+  };
 
-	update = async (id, payload) => {
-		const data = await this.db.member.update({
-			where: { id },
-			data: payload,
-		});
-		return data;
-	};
+  update = async (id, payload) => {
+    const data = await this.db.member.update({ where: { id }, data: payload });
+    return data;
+  };
 
-	delete = async (id) => {
-		const data = await this.db.member.delete({ where: { id } });
-		return data;
-	};
+  delete = async (id) => {
+    const data = await this.db.member.delete({ where: { id } });
+    return data;
+  };
 
-	extendDataSiswa = async (payload) => {
-		const { memberId, name, dateOfBirth, ...rest } = payload;
+  extendDataSiswa = async (payload) => {
+    const id = payload.memberId
+    return await this.db.$transaction(async (prisma) => {
+      const { name, profileImage, ...data } = payload
+      await prisma.memberIdentity.upsert({ where: { memberId: id }, create: data, update: data })
+      await prisma.member.update({ where: { id }, data: { name, memberState: memberConstant.memberState.Data_Ibu, profileImage } })
+    })
+  }
 
-    
-		const dob = new Date(dateOfBirth);
-		if (isNaN(dob.getTime())) throw new Error('Invalid dateOfBirth');
+  formatUIDParent = (memberId, relation) => {
+    return `${memberId}|${relation}`
+  }
 
-		return this.db.$transaction(async (prisma) => {
-			await prisma.memberIdentity.upsert({
-				where: { memberId },
-				create: { memberId, ...rest, dateOfBirth: dob },
-				update: { ...rest, dateOfBirth: dob },
-			});
+  extendDataIbu = async (payload) => {
+    const id = payload.memberId
+    const uid = this.formatUIDParent(id, "I")
+    payload['uid'] = uid
+    return await this.db.$transaction(async (prisma) => {
+      await prisma.memberParent.upsert({ where: { memberId: id, uid, relation: "I" }, create: { ...payload, relation: "I" }, update: { ...payload, relation: "I" } })
+      await prisma.member.update({ where: { id }, data: { memberState: memberConstant.memberState.Data_Ayah } })
+    })
+  }
 
-			await prisma.member.update({
-				where: { id: memberId },
-				data: {
-					name,
-					memberState: memberConstant.memberState.Data_Ibu,
-				},
-			});
-		});
-	};
+  extendDataAyah = async (payload) => {
+    const id = payload.memberId
+    const uid = this.formatUIDParent(id, "A")
+    payload['uid'] = uid
+    return await this.db.$transaction(async (prisma) => {
+      await prisma.memberParent.upsert({ where: { memberId: id, uid, relation: "A" }, create: { ...payload, relation: "A" }, update: { ...payload, relation: "A" } })
+      await prisma.member.update({ where: { id }, data: { memberState: memberConstant.memberState.Data_Wali } })
+    })
+  }
 
-	formatUIDParent = (memberId, relation) => {
-		return `${memberId}|${relation}`;
-	};
+  extendDataWali = async (payload) => {
+    const id = payload.memberId
+    const uid = this.formatUIDParent(id, "W")
+    payload['uid'] = uid
+    if (payload.parentAsGuardian) return await this.db.$transaction(async (prisma) => {
+      await prisma.memberIdentity.update({ where: { memberId: id }, data: { isParentGuardian: true } })
+      await prisma.member.update({ where: { id }, data: { memberState: memberConstant.memberState.Pilih_Kursus } })
+    })
+    delete payload.parentAsGuardian
+    return await this.db.$transaction(async (prisma) => {
+      await prisma.memberParent.upsert({ where: { memberId: id, uid, relation: "W" }, create: { ...payload, relation: "W" }, update: { ...payload, relation: "W" } })
+      await prisma.member.update({ where: { id }, data: { memberState: memberConstant.memberState.Pilih_Kursus } })
+    })
+  }
 
-	extendDataIbu = async (payload) => {
-		const id = payload.memberId;
-		const uid = this.formatUIDParent(id, 'I');
-		payload['uid'] = uid;
-		return await this.db.$transaction(async (prisma) => {
-			await prisma.memberParent.upsert({
-				where: { memberId: id, uid, relation: 'I' },
-				create: { ...payload, relation: 'I' },
-				update: { ...payload, relation: 'I' },
-			});
-			await prisma.member.update({
-				where: { id },
-				data: { memberState: memberConstant.memberState.Data_Ayah },
-			});
-		});
-	};
+  extendDataTraining = async (payload) => {
+    const id = payload.memberId
+    return await this.db.$transaction(async (prisma) => {
+      const categoryData = await prisma.trainingCategory.findFirst({ where: { id: payload.trainingId } })
+      if (!categoryData) throw new BadRequest("Data Kategori tidak ditemukan")
+      return await prisma.member.update({ where: { id }, data: { courseCategoryId: payload.courseCategoryId,  totalCoursePrice: 2000000, totalCourses: 1, courseLevel: payload.courseLevel } })
+    })
+  }
 
-	extendDataAyah = async (payload) => {
-		const id = payload.memberId;
-		const uid = this.formatUIDParent(id, 'A');
-		payload['uid'] = uid;
-		return await this.db.$transaction(async (prisma) => {
-			await prisma.memberParent.upsert({
-				where: { memberId: id, uid, relation: 'A' },
-				create: { ...payload, relation: 'A' },
-				update: { ...payload, relation: 'A' },
-			});
-			await prisma.member.update({
-				where: { id },
-				data: { memberState: memberConstant.memberState.Data_Wali },
-			});
-		});
-	};
-
-	extendDataWali = async (payload) => {
-		const id = payload.memberId;
-		const uid = this.formatUIDParent(id, 'W');
-		payload['uid'] = uid;
-		if (payload.parentAsGuardian)
-			return await this.db.$transaction(async (prisma) => {
-				await prisma.memberIdentity.update({
-					where: { memberId: id },
-					data: { isParentGuardian: true },
-				});
-				await prisma.member.update({
-					where: { id },
-					data: {
-						memberState: memberConstant.memberState.Pilih_Kursus,
-					},
-				});
-			});
-		delete payload.parentAsGuardian;
-		return await this.db.$transaction(async (prisma) => {
-			await prisma.memberParent.upsert({
-				where: { memberId: id, uid, relation: 'W' },
-				create: { ...payload, relation: 'W' },
-				update: { ...payload, relation: 'W' },
-			});
-			await prisma.member.update({
-				where: { id },
-				data: { memberState: memberConstant.memberState.Pilih_Kursus },
-			});
-		});
-	};
-
-	extendDataTraining = async (payload) => {
-		const id = payload.memberId;
-		return await this.db.$transaction(async (prisma) => {
-			const trainingData = await prisma.training.findFirst({
-				where: { id: payload.trainingId },
-			});
-			if (!trainingData)
-				throw new BadRequest('Data Training tidak ditemukan');
-			await prisma.memberCourse.update({
-				where: { memberId: id },
-				data: {
-					memberId: id,
-					trainingId: trainingData.id,
-					status: 'Sedang',
-				},
-			});
-			await prisma.member.update({
-				where: { id },
-				data: {
-					currentCourseId: payload.trainingId,
-					totalCoursePrice: '2000000',
-					totalCourses: 1,
-				},
-			});
-		});
-	};
-
-	extendDataPembayaran = async (payload) => {
-		const id = payload.memberId;
-		return await this.db.$transaction(async (prisma) => {
-			await prisma.memberTransaction.create({
-				data: {
-					memberId: id,
-					paymentTotal: user.member.totalCousePrice,
-				},
-			});
-		});
-	};
+  extendDataPembayaran = async (payload) => {
+    const createdPayment = await this.paymentService.createPayment({ ...payload, paymentTotal: 2000000,  purpose: "Pendaftaran", status: "Tunda"})
+    const { paymentMethod, paymentTotal, qrisLink, virtualAccountNo, expiredDate, ...rest } = createdPayment
+    return { paymentMethod, paymentTotal, qrisLink, virtualAccountNo, expiredDate }
+  }
 }
 
-export default memberService;
+export default memberService;  
--- src/core/training/training.validator.js
+++ src/core/training/training.validator.js
@@ -1,8 +1,15 @@
 import Joi from "joi";
+import { type } from "os";
 
 export const trainingValidator = {
   create: Joi.object({
-    // no-data
+     title: Joi.string().required(),
+     description: Joi.string().required(),
+     trainingImage: Joi.string().required(),
+     type: Joi.string().valid("R", "P").required(),
+     categoryId: Joi.number().integer().required(),
+     totalParticipants: Joi.number().integer().required(),
+     
   }),
   update: Joi.object({
     // no-data
